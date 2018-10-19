3a185b71388a359c8d69f58ae453668e6b491987
bratizgut <43445560+bratizgut@users.noreply.github.com>
2018-10-19 22:49:42 +0700

    FIle copy

---
+++ dz.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+ 
+long int filesize( FILE *fp ){
+    long int size;
+	fseek(fp, 0, SEEK_END);
+    size = ftell( fp );
+	fseek(fp, 0, SEEK_SET);
+    return (size);
+}
+ 
+void main(){
+    FILE *fp;
+	FILE *OUT1;
+	FILE *OUT2;
+	unsigned long size, i;
+	char sym;
+
+    fp = fopen( "input.txt", "r" );
+	OUT1 = fopen( "out1.txt", "w" );
+	OUT2 = fopen( "out2.txt", "w" );
+    if( (fp != NULL) && (OUT1 != NULL) && (OUT2 != NULL) ){
+		size = filesize(fp);
+		for (i = 1; i <= size/2; i++){
+			sym = fgetc(fp);
+			printf("%c", sym);
+			putc(sym, OUT1);
+		}
+		for (i; i <= size; i++){
+			sym = getc(fp);
+			if (sym != EOF)
+				putc(sym, OUT2);
+		}
+		fclose(fp);
+		fclose(OUT1);
+		fclose(OUT2);
+    } else {
+		printf("error!!!");
+	}
+}
