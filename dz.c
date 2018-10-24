a233a4976dfc4c5d522b404aa4be102c0299151a
bratizgut <43445560+bratizgut@users.noreply.github.com>
2018-10-24 18:16:42 +0700

    Fixed(added file check)

--- dz.c
+++ dz.c
@@ -8,15 +8,17 @@ long int filesize( FILE *fp ){
     return (size);
 }
  
-void main(){
-    FILE *fp, *OUT1, *OUT2;
+int main(void){
+    FILE *fp, *OUT1, *OUT2, *RES;
 	unsigned long size, i;
 	char sym;
 
     fp = fopen( "input.txt", "r" );
-	OUT1 = fopen( "out1.txt", "w" );
-	OUT2 = fopen( "out2.txt", "w" );
-    if( (fp != NULL) && (OUT1 != NULL) && (OUT2 != NULL) ){
+	OUT1 = fopen( "out1.txt", "w + t" );
+	OUT2 = fopen( "out2.txt", "w + t" );
+	RES = fopen( "result.txt", "w + t" );
+	
+    if( (fp != NULL) && (OUT1 != NULL) && (OUT2 != NULL) && (RES != NULL) ){
 		size = filesize(fp);
 		for (i = 1; i < size; i++){
 			sym = fgetc(fp);
@@ -27,8 +29,34 @@ void main(){
 					putc(sym, OUT2);
 			}
 		}
-		fcloseall;
+		size = filesize(OUT1);
+		for (i = 1; i < size; i++){
+			sym = fgetc(OUT1);
+			if (sym != EOF)
+					putc(sym, RES);
+		}
+		size = filesize(OUT2);
+		for (i = 1; i < size; i++){
+			sym = fgetc(OUT2);
+			if (sym != EOF)
+					putc(sym, RES);
+		}
+		filesize(fp);
+		fseek(RES, 0, SEEK_SET);
+		for (i = 1; i < size; i++){
+			if (fgetc(fp) != fgetc(RES)){
+				printf("Error in result \n");
+				printf("%ld", i);
+				return 0;
+			}
+		}
+		printf("Seccesful!!!");
+		fclose(fp);
+		fclose(OUT1);
+		fclose(OUT2);
+		fclose(RES);
     } else {
 		printf("error!!!");
 	}
+	return 0;
 }
